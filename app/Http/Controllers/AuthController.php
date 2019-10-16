3f672a17ba6631b77618de6f259668aa8c1678d2
alexsanro <alexsanro1@gmail.com>
2019-10-16 17:25:47 +0200

    Make some tests Fixed autocomplete for laravel on vscode Change to 12 length varchar of password user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -76,8 +76,10 @@ class AuthController extends Controller
     public function logout(Request $request)
     {
         $request->user()->token()->revoke();
-        return response()->json(['message' =>
-        'Successfully logged out']);
+        return response()->json(
+            ['message' =>
+                'Successfully logged out'
+            ]);
     }
 
     /**
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withHeaders([
+            'Accept' => 'aplication/json',
+            'Content-type' => 'aplication/json',
+            'X-Requested-With' => 'XMLHttpRequest'
+        ]);
+    }
+
+    /**
+     * Test nuevo usuario OK
+     */
+    public function testSignUpValidationError(){
+        $response = $this->json(
+            'POST',
+            '/api/signup',
+            [
+                'email' => 'prueba1@gmail.com',
+                'password' => '1234567890123'
+            ]
+        );
+
+        $response->assertStatus(422)->assertJsonFragment(['errors' => ['name' => "The name field is required."]]);
+    }
+
+    /**
+     * Test comprueba acceso Login erroneo
+     *
+     * @return void
+     */
+    public function testLoginErrorTest()
+    {
+        $response = $this->json(
+            'POST',
+            '/api/login',
+            [
+                'email' => 'prueba1@gmail.com',
+                'password' => 'alexsanro'
+            ]
+        );
+
+        $response->assertStatus(401);
+    }
+}
