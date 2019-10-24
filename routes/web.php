39b60721addeed9a5300b7bd8cc5e33dc61aa8d3
alexsanro <alexsanro1@gmail.com>
2019-10-24 16:30:45 +0200

    Refactorizado archivos y modificado llos archivos que no deben de salir en el sonar.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::fallback(function () {
+    return response()->json(['message' => 'Not Found.'], 404);
+})->name('api.fallback.404');
